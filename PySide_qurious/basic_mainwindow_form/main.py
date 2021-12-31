3092bf5dd77ec4a311753a9139a53b7d5bc9b2a9
skyprawngo <skyprawngo@gmail.com>
2021-12-31 20:54:43 +0900

    Update main.py

--- PySide_qurious/basic_mainwindow_form/main.py
+++ PySide_qurious/basic_mainwindow_form/main.py
@@ -5,11 +5,10 @@ from PySide6.QtCore import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        
 
 if __name__ == "__main__":
     app = QApplication()
-    window = QMainWindow()
+    window = MainWindow()
     window.show()
     app.exec()
     
