66d84b9c05fd21168e3cc44480ba7354cf0e5dde
skyprawngo <skyprawngo@gmail.com>
2022-01-07 19:21:50 +0900

    Update learn2.py

--- BeautifulSoup_learn/learn2.py
+++ BeautifulSoup_learn/learn2.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-url ='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%99%98%EC%9C%A8%EC%A1%B0%ED%9A%8C'
-
-tables = pd.read_html(url)
-print(tables)
-print(tables[0])
+class Function_exchange_rate:
+    url ='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%99%98%EC%9C%A8%EC%A1%B0%ED%9A%8C'
+    tables = pd.read_html(url)[0]
+    print(tables)
+    
+    def USD_to_KRW():
+        return Function_exchange_rate.tables["매매기준율"][0]
+    
+    def JPY_to_KRW():
+        return Function_exchange_rate.tables["매매기준율"][1]
