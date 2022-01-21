e9f6a334aec83ac1c549b9ed518ef51c41c6cd4b
skyprawngo <skyprawngo@gmail.com>
2022-01-22 00:19:28 +0900

    Update arbitrage.py

--- ccxt_learn/strategy/arbitrage.py
+++ ccxt_learn/strategy/arbitrage.py
@@ -37,7 +37,7 @@ def dailydo():
             coin_list.append(wallet_coin)
             
     coin_list = list(set(ideal_rate.keys())|set(coin_list))
-    df_coin = pd.DataFrame(index=coin_list)
+    df_BUSD = pd.DataFrame(index=coin_list)
     ask = []
     amounts = []
     total_price = 0
@@ -49,29 +49,30 @@ def dailydo():
             ticker["ask"] = 1
         amount = current_balance[coin]["total"]
         ask.append(ticker["ask"])
-        df_coin.loc[coin,"ask"] = ticker["ask"]
-        df_coin.loc[coin,"amount"] = amount
-        df_coin.loc[coin,"value(USD)"] = ticker["ask"]*amount
+        df_BUSD.loc[coin,"ask"] = ticker["ask"]
+        df_BUSD.loc[coin,"amount"] = amount
+        df_BUSD.loc[coin,"value(USD)"] = ticker["ask"]*amount
         total_price += ticker["ask"]*amount
-    df = pd.concat([df_coin,df_rate], axis = 1)
+    df = pd.concat([df_BUSD,df_rate], axis = 1)
     df = df.fillna(0)
     current_rate = {}
     for coin in coin_list:
-        current_rate[coin] = df_coin.loc[coin,"value(USD)"]/total_price
+        current_rate[coin] = df_BUSD.loc[coin,"value(USD)"]/total_price
     df.insert(3, "current_rate", current_rate.values())
 
     df_transfer = pd.DataFrame()
     df_transfer["tr_rate"] = df["current_rate"] - df["ideal_rate"]
     print(df)
     print(total_price)
+    
     for coin in df_transfer.index:
-        if df_transfer.loc[coin, "tr_rate"] > 0:
-            df_transfer.at[coin, "tr_price(USD)"] = df_transfer.loc[coin, "tr_rate"] * df.loc[coin, "amount"] * df.loc[coin, "ask"]
-        else :
-            df_transfer.at[coin, "tr_price(USD)"] = 0
-    tr_sum = df_transfer["tr_price(USD)"].sum()
+        df_transfer.at[coin, "tr_price(USD)"] = int(df_transfer.loc[coin, "tr_rate"] * total_price)
+        if not df_transfer.loc[coin, "tr_price(USD)"] == 0:
+            df_transfer.at[coin, "tr_amount"] = df_transfer.loc[coin, "tr_price(USD)"] / df.loc[coin, "ask"]
+
     print(df_transfer)
-    print(tr_sum)
+    
+    
     pass
 
 if __name__ == "__main__":
