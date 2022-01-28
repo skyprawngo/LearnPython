128479a6a20c7b4af7e339c12a13e3e6b0ff293c
skyprawngo <skyprawngo@gmail.com>
2022-01-29 00:59:45 +0900

    .

--- ccxt_learn/strategy/MA/MA.py
+++ ccxt_learn/strategy/MA/MA.py
@@ -18,31 +18,24 @@ def get_account():
     })
     return binance
 
-def set_ohlcv_file(df = pd.DataFrame()):
+def set_ohlcv_file(df = pd.DataFrame(), filename = "ohlcv.csv"):
     file_path = os.path.dirname(os.path.abspath(__file__))
-    transaction_path = os.path.normpath(os.path.join(file_path,"ohlcv.csv"))
+    transaction_path = os.path.normpath(os.path.join(file_path, filename))
     if not os.path.isfile(transaction_path):
         df.to_csv(transaction_path)
     if not df.empty:
         df.to_csv(transaction_path)
 
-def get_ohlcv_file():
+def get_ohlcv_file(filename = "ohlcv.csv"):
     file_path = os.path.dirname(os.path.abspath(__file__))
-    transaction_path = os.path.normpath(os.path.join(file_path,"ohlcv.csv"))
+    transaction_path = os.path.normpath(os.path.join(file_path, filename))
     df = pd.read_csv(transaction_path)
     return df
 
-# 데이터를 가져올 날짜 설정 
-# start = datetime.datetime(2021, 1, 1) 
-# end = datetime.datetime(2021, 8, 20) 
-
-# 야후에서 삼성전자 주가 가져오기 
-# samsung = web.DataReader("005930.KS", "yahoo", start, end) 
-
 binance = get_account()
 
-if False:
-    ohlcv = binance.fetch_ohlcv("BNB/BUSD", timeframe="4h")
+if True:
+    ohlcv = binance.fetch_ohlcv("ETC/BUSD", timeframe="4h")
     df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df.set_index('timestamp', inplace=True)
     set_ohlcv_file(df)
@@ -55,7 +48,7 @@ df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
 
 # MA백태스팅 *.shift()를 사용해서도 한번 만들어볼것
-for mnav in [10, 20, 30, 40, 50, 60]:
+for mnav in [10, 20, 30, 40, 50]:
     for i in range(len(df.index)):
         if i < mnav:
             continue
@@ -66,6 +59,8 @@ df['10-20'] = df[10]-df[20]
 df["Buy"] = (df['close-50']>0)&(df['close-50'].shift()<0)
 df["Sell"] = (df['10-20']<0)&(df['10-20'].shift()>0)
 
+# set_ohlcv_file(df, "bos_BNB.csv")
+
 profit = 0
 tradetime = 0
 buyed = None
@@ -73,10 +68,10 @@ selled = None
 for i in range(len(df.index)):
     if df.at[i, 'Buy']:
         buyed = df.at[i, 'close']
-    if df.at[i, 'Sell']:
+    if df.at[i, 'Sell'] and buyed:
         selled = df.at[i, 'close']
     if buyed and selled:
-        profit += buyed-selled
+        profit += selled-buyed
         tradetime += 1
         buyed = None
         selled = None
@@ -88,7 +83,7 @@ print(tradetime)
 df.set_index('timestamp', inplace=True)
 mpf.plot(df, 
         type='candle', # 캔들 차트 
-        mav=(10, 20, 30, 40, 50, 60), # 10일, 20일, 60일 이동평균선 표시 
+        mav=(10, 20, 30, 40, 50), # 10일, 20일, 60일 이동평균선 표시 
         volume=True, #거래량 표시 
         style='yahoo', 
         figratio=(10,5),
